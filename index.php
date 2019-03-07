86df03571f9c7969d024801b3f573688eec52e55
ethel <pruss.ethel@gmail.com>
2019-03-07 16:26:25 +0100

    buttons n stuff

--- index.php
+++ index.php
@@ -3,24 +3,23 @@
  include "headerNav.php";
 ?>
 
-<div class="jumbotron"></div>
-<section class="mb-4">
+<div class="jumbotron" id="spacer"></div>
+<div class="container w-60 align-items-center justify-content-center" id="feature">
 <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
 <ol class="carousel-indicators">
     <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
     <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
     <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
   </ol>
- 
 <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="d-block w-100 img-rounded" src="/bookstore/images/slide1.jpg" alt="First slide">
+    <img class="d-block w-100" src="/bookstore/images/slide1.jpg" alt="First slide">
+</div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="/bookstore/images/slide2.jpg" alt="Second slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100 img-rounded" src="/bookstore/images/slide2.jpg" alt="Second slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100 img-rounded" src="/bookstore/images/slide3.jpg" alt="Third slide">
+      <img class="d-block w-100" src="/bookstore/images/slide3.jpg" alt="Third slide">
     </div>
   </div>
   <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
@@ -31,6 +30,8 @@
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="sr-only">Next</span>
   </a>
+</div>
+</div>
 </div>
 </section>
 
